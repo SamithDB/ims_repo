47875442fd3f16841a22cd13ea2756e1d196a691
sbasnayaka <samith.basnayaka@cinglevue.com>
2019-11-05 12:17:45 +0530

    fixes

--- config/passport.js
+++ config/passport.js
@@ -34,7 +34,7 @@ module.exports = function(passport) {
 
     // used to deserialize the user
     passport.deserializeUser(function(id, done) {
-        sql.close();
+        mysql.close();
         connection = mysql.createConnection(dbconfig.connection);
         connection.query("SELECT * FROM login WHERE idlogin = ? ",[id], function(err, rows){
             done(err, rows[0]);
@@ -56,7 +56,7 @@ module.exports = function(passport) {
             passReqToCallback : true // allows us to pass back the entire request to the callback
         },
         function(req, username, password, done) {
-            sql.close();
+            mysql.close();
             connection = mysql.createConnection(dbconfig.connection);
             // find a user whose email is the same as the forms email
             // we are checking to see if the user trying to login already exists
@@ -121,7 +121,7 @@ module.exports = function(passport) {
             passReqToCallback : true // allows us to pass back the entire request to the callback
         },
         function(req, username, password, done) { // callback with email and password from our form
-            sql.close();
+            mysql.close();
             connection = mysql.createConnection(dbconfig.connection);
             connection.query("SELECT * FROM login WHERE username = ?",[username], function(err, rows){
                 if (err)
